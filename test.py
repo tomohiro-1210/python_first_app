3f07edf0a0d6dc863ea076df0e565fed2d9851f0
tomohiro_0225 <express22212270@gmail.com>
2024-07-17 13:12:00 +0900

    Flaskのインストールと仮想環境構築完了

--- test.py
+++ test.py
@@ -2,3 +2,16 @@ print("これがgithubに行ったら無事にpushされている証拠")
 
 sanin_trains = ["やくも", "スーパーいなば" ,"スーパーまつかぜ", "スーパーおき" , "スーパーはくと", "はまかぜ"]
 print(sanin_trains)
+
+django = "Djangoは大規模なアプリ開発に向いているFW。"
+flask = "Flaskは小規模アプリ開発にむいているFW"
+
+#仮想環境とFlaskバージョンはセット。
+#別のFlaskバージョンで開発する場合はまた仮想環境構築が必要になってくる。
+#仮想環境構築方法> https://msiz07-flask-docs-ja.readthedocs.io/ja/latest/installation.html
+
+#※powerShellで仮想環境構築が管理者権限で実行できないときは下記コード
+# Set-ExecutionPolicy RemoteSigned -Scope CurrentUser
+
+# 管理者権限を元に戻すときは下記コードでセキュリティーを戻す
+# Set-ExecutionPolicy Restricted -Scope CurrentUser
