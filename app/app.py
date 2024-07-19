9292d9d49af0cefb424fcc604c2303397708939e
tomohiro_0225 <express22212270@gmail.com>
2024-07-19 12:47:37 +0900

    データーベースの作成とHTMLとの接続

--- app/app.py
+++ app/app.py
@@ -1,29 +1,28 @@
-from flask import Flask , render_template #render_templateはフォルダの読み込みに必要
+from flask import Flask , render_template,g #render_templateはフォルダの読み込みに必要
+import sqlite3
+DATABASE="flaskmemo2.db"
 
 app = Flask(__name__)
 
-# リスト、ここは分けたほうがよさそうだなという印象。
-list = [
-    'test0',
-    'test1',
-    'test2',
-    'test3',
-    'test4',
-    'test5',
-    'test6',
-    'test7',
-]
 
 # 127・・・/<name>でURLを入力したとき関数で返せるようにする
-@app.route("/<name>")
-def H(name):
-    return render_template('index.html', name=name, list=list) 
-    # Wクォートではなくシングルクォートで囲む
-
-# topページを表示させる場合
-# @app.route("/")
-# def H_top():
-#     return render_template('index.html') # Wクォートではなくシングルクォートで囲む
+@app.route("/")
+def top():
+    memo_list = get_db().execute("select id, title, body from memo").fetchall()
+    return render_template('index.html', memo_list=memo_list) 
 
 if __name__ == "__main__":
     app.run()
+
+    
+#DBへアクセス
+def connect_db():
+    rv = sqlite3.connect(DATABASE)
+    rv.row_factory = sqlite3.Row
+    return rv
+
+def get_db():
+    if not hasattr(g, 'sqlite_db'):
+        g.sqlite_db = connect_db()
+    return g.sqlite_db
+
